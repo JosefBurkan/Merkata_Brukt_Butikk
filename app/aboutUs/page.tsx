c0339ffaf274c1efe1aab10b241dc1bc813f96bc
tastebutter <josefbu@uia.no>
2026-08-12 15:38:46 +0200

    La til en knapp på hjemmesiden som tar deg til aboutUs

--- app/aboutUs/page.tsx
+++ app/aboutUs/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function AboutUS() {
+
   return (
+
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Merkatå bruktbutikk{" "}
+            Om oss{" "}
             <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
               Homepage.tsx
             </code>{" "}
