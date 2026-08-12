e9184b1476a2bdd532ec3b05cab2e9244b2a4e85
Kenny Vo <kenvo2201@hotmail.com>
2026-08-12 15:51:45 +0200

    Made changes to the pages

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function About() {
+
+  return (
+    <main>
+      <h1>About Us</h1>
+      <p>Welcome to our about page!</p>
+    </main>
+
+  );
+}
