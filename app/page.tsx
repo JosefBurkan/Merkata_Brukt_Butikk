73b7556cedba0cfc7c4457a56681b8c84621f9d3
tastebutter <josefbu@uia.no>
2026-08-13 14:06:20 +0200

    oppdaterte hjemmesiden litt, la til bilde og fikset posisjoner. Øver fortsatt

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,23 @@ export default function Home() {
   return (
       <main>
         <h1 style={{ color: "black", marginTop: "10px" }}>Velkommen til Merkatå bruktbutikk</h1>
-        <p style={{ color: "black", marginTop: "10px" }}>Hos oss finner du et spennende utvalg av brukte skatter, vintage, retro, antikviteter og unike gjenstander med historie. 
-
-            Vi tror at gjenbruk handler om mer enn å spare penger. Det handler om å ta vare på kvalitet, redusere unødvendig forbruk og gi gamle favoritter en ny eier.</p>
+      <p style={{ color: "black", marginTop: "10px" }}>
+        
+        <br/>Hos oss finner du et spennende utvalg av brukte skatter, vintage, retro, antikviteter og unike gjenstander med historie. 
+        <br/> Vi tror at gjenbruk handler om mer enn å spare penger.
+        <br/> Det handler om å ta vare på kvalitet, redusere unødvendig forbruk og gi gamle favoritter en ny eier.
+      </p>
           <button style={{ color: "black", marginTop: "10px" }}
           type="button"
           onClick={() => router.push('/about')}
         >
           Go to About Us
-        </button>
+      </button>
+      
+      <img
+        class = "h-128 w-128 overflow-hidden absolute right-0 top-0"
+        src = "/forside_bilde.jpg"
+      />
       </main>
   );
 }
