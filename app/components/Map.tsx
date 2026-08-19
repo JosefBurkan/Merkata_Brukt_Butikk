27fef5c6dd41378427a1b6ae98dbccdec5fc807e
tastebutter <josefbu@uia.no>
2026-08-19 16:23:55 +0200

    Har kart på forsiden nå

---
+++ app/components/Map.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { GoogleMapsEmbed } from '@next/third-parties/google'
+
+const mapsKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+export default function Map()
+{
+    return (
+        <GoogleMapsEmbed
+            apiKey={mapsKey}
+            height={400}
+            width="100%"
+            mode="place"
+            q="Oslo, Schweigaards gate 92"
+        />
+    );
+}
