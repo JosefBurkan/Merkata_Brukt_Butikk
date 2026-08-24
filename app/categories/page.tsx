cc7d39ba5d141c3771030765cd04984001eadb87
tastebutter <josefbu@uia.no>
2026-08-24 22:19:25 +0200

    fikset dynamiske sider for produkter nå

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,16 +1,28 @@
+"use client"
+
+import { useRouter } from 'next/navigation'
+
+
 export default function Categories() {
+
+    const router = useRouter();
+
+    const dynamicRoute = (name) =>
+    {
+        router.push(`products/${name}`)
+    }    
+    
     return (
         <main className="categories">
             <section className="news-section">
                 <h2 className="pb-10">Kategorier</h2>
-
                 <div className="product-list grid grid-cols-1 lg:grid-cols-3 gap-50 justify-items-center mx-auto">
-                    <div className="product-card">elektronikk</div>
-                    <div className="product-card">møbler</div>
-                    <div className="product-card">fritid</div>
-                    <div className="product-card">klær</div>
-                    <div className="product-card">musikk</div>
-                    <div className="product-card">annet</div>
+                    <button onClick={() => dynamicRoute("elektronikk")} className="product-card">elektronikk</button>
+                    <button onClick={() => dynamicRoute("mobler")} className="product-card">møbler</button>
+                    <button onClick={() => dynamicRoute("fritid")} className="product-card">fritid</button>
+                    <button onClick={() => dynamicRoute("klaer")} className="product-card">klær</button>
+                    <button onClick={() => dynamicRoute("musikk")} className="product-card">musikk</button>
+                    <button onClick={() => dynamicRoute("annet")} className="product-card">annet</button>
                 </div>
             </section>
         </main>
