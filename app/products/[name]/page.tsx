17c84ebabeb56d4f3e094c65beb2d92c1473d6b3
tastebutter <josefbu@uia.no>
2026-09-04 17:22:48 +0200

    push for deploy

--- app/products/[name]/page.tsx
+++ app/products/[name]/page.tsx
@@ -203,7 +203,7 @@ export default async function Products({
         </nav>
 
 
-        {/* =========================
+        {/* ========================= 
             PAGE HEADER
         ========================= */}
 
