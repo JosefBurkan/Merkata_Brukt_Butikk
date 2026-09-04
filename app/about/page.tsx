5be6e3edff71c781b26c1faf266370bd706ff7ad
Kenny Vo <kenvo2201@hotmail.com>
2026-09-04 16:55:01 +0200

    Fixed some bugs

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,25 +6,29 @@ export default function About() {
       <div className="mx-auto w-full max-w-6xl px-6 py-12 md:px-10 md:py-16 lg:px-12">
 
       {/* Breadcrumbs */}
-<nav
-  aria-label="Breadcrumb"
-  className="mb-8 flex flex-wrap items-center gap-2 text-sm"
->
-  <Link
-    href="/"
-    className="opacity-70 transition hover:opacity-100"
-  >
-    Forside
-  </Link>
+      <nav
+          aria-label="Breadcrumb"
+          className="mb-8 flex flex-wrap items-center gap-2 text-sm"
+        >
+          <Link
+            href="/"
+            className="opacity-70 transition hover:opacity-100"
+          >
+            Forside
+          </Link>
 
-  <span className="opacity-40">
-    /
-  </span>
+          <span className="opacity-40">
+            /
+          </span>
 
-  <span className="font-semibold">
-    Om oss
-  </span>
-</nav>
+          <Link
+                        href="/about"
+                        className="font-semibold"
+                    >
+                        Om oss
+                    </Link>
+
+        </nav>
         
 
         {/* =========================
